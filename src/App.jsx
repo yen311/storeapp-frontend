99ae1c627aaf63c2ebe829163f0293ef51542c4f
Yen Kuo <u7075575@anu.edu.au>
2022-06-23 21:52:18 +0930

    add btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>Hello React 123</div>;
+  return (
+    <div className='App'>
+      <p>Hello React 123</p>
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            "https://storeapp-api.herokuapp.com/api/",
+          );
+          let data = await response.json();
+          console.log(data);
+        }}
+      >
+        Get Data
+      </button>
+    </div>
+  );
 }
 
 export default App;
